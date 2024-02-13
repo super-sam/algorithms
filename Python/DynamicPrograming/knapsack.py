243c5de3d3603d09082b25160de392eecf392239
Supratim Samantray <supratim.samantray@gmail.com>
2024-02-13 15:46:30 +0530

    feat: top down knapsack

--- Python/DynamicPrograming/knapsack.py
+++ Python/DynamicPrograming/knapsack.py
@@ -38,6 +38,26 @@ def max_profit_memo(weights: List[int], values: List[int], capacity: int):
         pprint(memo)
         return memo[nsize][capacity]
     return max_profit_util(capacity, len(weights) - 1)
+
+def max_profit_topdown(weights: List[int], values: List[int], capacity: int) -> int:
+    dp = [
+        [ -1 for _ in range(capacity+1)]
+        for _ in range(len(weights) + 1)
+    ]
+    for n in range(len(dp)):
+        for w in range(len(dp[0])):
+            if n == 0 or w == 0:
+                dp[n][w] = 0
+                continue
+            if weights[n-1] <= w:
+                dp[n][w] = max(
+                    values[n-1] + dp[n-1][w - weights[n-1]],
+                    dp[n-1][w]
+                )
+            else:
+                dp[n][w] = dp[n-1][w]
+            
+    return dp[len(weights)][capacity]
         
 if __name__ == '__main__':
     print(max_profit([1, 3, 4, 5], [1, 4, 5, 7], 7))
