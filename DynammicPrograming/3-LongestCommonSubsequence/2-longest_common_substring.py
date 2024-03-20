97b24ed1927236960e96c3078f6115ff32425edc
Supratim Samantray <supratim.samantray@gmail.com>
2024-03-20 08:17:12 +0530

    lcs

--- DynammicPrograming/3-LongestCommonSubsequence/2-longest_common_substring.py
+++ DynammicPrograming/3-LongestCommonSubsequence/2-longest_common_substring.py
@@ -12,22 +12,23 @@ def longest_common_substring(s1, s2):
     return longest_common_substring_util(len(s1), len(s2))
 
 def longest_common_substring_memoisation(s1, s2):
-    dp = [
-        [-1 for _ in range(len(s2) + 1)]
-        for _ in range(len(s1) + 1)
-    ]
+    dp = {} # {(n1, n2, count): result}
+
     def longest_common_substring_util(n1, n2, count=0):
         if n1 == 0 or n2 == 0:
             return count
-        if dp[n1][n2] != -1:
-            return dp[n1][n2]
+        if (n1, n2, count) in dp:
+            return dp[(n1, n2, count)]
+
         if s1[n1 - 1] == s2[n2 - 1]:
-            dp[n1][n2] = longest_common_substring_util(n1 - 1, n2 - 1, count + 1)
+            c = longest_common_substring_util(n1 - 1, n2 - 1, count + 1)
         else:
-            dp[n1][n2] = max(count, 
-                             max(longest_common_substring_util(n1 - 1, n2, 0), 
-                                 longest_common_substring_util(n1, n2 - 1, 0)))
-        return dp[n1][n2]
+            c = max(count, longest_common_substring_util(n1 - 1, n2, 0), longest_common_substring_util(n1, n2 - 1, 0))
+        
+        dp[(n1, n2, count)] = c
+        return dp[(n1, n2, count)]
+    
+    
     return longest_common_substring_util(len(s1), len(s2))
 
 def longest_common_substring_topdown(s1, s2):
@@ -55,3 +56,7 @@ if __name__ == "__main__":
     print(longest_common_substring(txt, pat)) # 4
     print(longest_common_substring_memoisation(txt, pat)) # 4
     print(longest_common_substring_topdown(txt, pat)) # 4
+    print(longest_common_substring("yourocks", "youareawesome")) # 3
+    print(longest_common_substring("12321", "32147")) # 3
+    print(longest_common_substring_memoisation("12321", "32147")) # 3
+    print(longest_common_substring_topdown("12321", "32147")) # 3
---
+++ DynammicPrograming/3-LongestCommonSubsequence/3-longest_common_subsequence_print.py
@@ -0,0 +1,58 @@
+def print_longest_common_subsequence(s1, s2):
+    def print_longest_common_subsequence_util(n1, n2):
+        if n1 == 0 or n2 == 0:
+            return ""
+        elif s1[n1 - 1] == s2[n2 - 1]:
+            return print_longest_common_subsequence_util(n1 - 1, n2 - 1) + s1[n1 - 1]
+        else:
+            if n1 > n2:
+                return print_longest_common_subsequence_util(n1 - 1, n2)
+            else:
+                return print_longest_common_subsequence_util(n1, n2 - 1)
+    return print_longest_common_subsequence_util(len(s1), len(s2))
+
+def print_longest_common_subsequence_memoisation(s1, s2):
+    dp = [
+        [" " for _ in range(len(s2) + 1)]
+        for _ in range(len(s1) + 1)
+    ]
+    def print_longest_common_subsequence_util(n1, n2):
+        if n1 == 0 or n2 == 0:
+            return ""
+        elif dp[n1][n2] != " ":
+            return dp[n1][n2]
+        elif s1[n1 - 1] == s2[n2 - 1]:
+            dp[n1][n2] = print_longest_common_subsequence_util(n1 - 1, n2 - 1) + s1[n1-1]
+        else:
+            if n1 > n2:
+                dp[n1][n2] = print_longest_common_subsequence_util(n1 - 1, n2)
+            else:
+                dp[n1][n2] = print_longest_common_subsequence_util(n1, n2 - 1)
+        return dp[n1][n2]
+    return print_longest_common_subsequence_util(len(s1), len(s2))
+
+def print_longest_common_subsequence_topdown(s1, s2):
+    dp = [
+        [" " for _ in range(len(s2) + 1)]
+        for _ in range(len(s1) + 1)
+    ]
+
+    for n1 in range(len(s1) + 1):
+        for n2 in range(len(s2) + 1):
+            if n1 == 0 or n2 == 0:
+                dp[n1][n2] = ""
+            elif s1[n1 -1] == s2[n2 - 1]:
+                dp[n1][n2] = dp[n1 -1][n2 - 1] + s1[n1 -1]
+            elif n1 > n2:
+                dp[n1][n2] = dp[n1 - 1][n2]
+            else:
+                dp[n1][n2] = dp[n1][n2 - 1]
+    return dp[len(s1)][len(s2)]
+
+if __name__ == "__main__":
+    s1 = "acbcf"
+    s2 = "abcdaf"
+    print(print_longest_common_subsequence(s1, s2)) # yourocks
+    print(print_longest_common_subsequence_memoisation(s1, s2))
+    print(print_longest_common_subsequence_topdown(s1, s2)) # yourocks
+    
